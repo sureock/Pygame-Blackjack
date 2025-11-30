26de4425811764b761bce7af3ec5f24d9ccba470
V1rysn1k <ivan.voronko718@gmail.com>
2025-11-30 15:15:35 +0300

    Изменён внешний вид кнопок

--- blackjack/game.py
+++ blackjack/game.py
@@ -4,13 +4,11 @@ width, height = 1000, 750
 fps = 24
 
 white = (255,255,255)
-gray_1 = (71,74,81)
-gray_2 = (160,160,164)
 black = (0,0,0)
 
 pygame.init()
 
-button_surface = pygame.Surface((150,50))
+button_surface = pygame.Surface((250,50))
 
 screen = pygame.display.set_mode((width,height))
 pygame.font.match_font('font.otf')
@@ -22,34 +20,27 @@ text = pygame.image.load('2(900x270).png').convert_alpha()
 text_rect = text.get_rect()
 text_rect.topleft = ((width - 900)//2,20)
 
-text_1 = font.render("PLAY",True,(255,255,255))
+text_1 = font.render("PLAY",True,white)
 text_1_rect = text_1.get_rect()
 text_1_rect.topleft =((width-90)//2,535)
 button_1_rect = pygame.Rect(
-    (width - 500)//2,
-    525,500,50)
+    (width - 120)//2,525,120,50)
 
-text_2 = font.render("SETUP",True,(255,255,255))
+text_2 = font.render("SETUP",True,white)
 text_2_rect = text_2.get_rect()
 text_2_rect.topleft =((width-120)//2,610)
 button_2_rect = pygame.Rect(
-    (width - 500)//2,
-    600,500,50)
+    (width - 120)//2,600,120,50)
 
-text_3 = font.render("EXIT",True,(255,255,255))
+text_3 = font.render("EXIT",True,white)
 text_3_rect = text_3.get_rect()
 text_3_rect.topleft =((width-90)//2,685)
 button_3_rect = pygame.Rect(
-    (width - 500)//2,
-    675,500,50)
+    (width - 120)//2,675,120,50)
 
 running = True
 while running:
     dt = clock.tick(fps)/1000
-
-    color_button_1 = gray_1
-    color_button_2 = gray_1
-    color_button_3 = gray_1
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -62,22 +53,13 @@ while running:
                 ...
             if button_3_rect.collidepoint(event.pos):
                 running = False
-    if button_1_rect.collidepoint(pygame.mouse.get_pos()):
-        pygame.draw.rect(screen, gray_1, button_1_rect)
-    else:
-        pygame.draw.rect(screen, gray_2, button_1_rect)
-    if button_2_rect.collidepoint(pygame.mouse.get_pos()):
-        pygame.draw.rect(screen, gray_1, button_2_rect)
-    else:
-        pygame.draw.rect(screen, gray_2, button_2_rect)        
-    if button_3_rect.collidepoint(pygame.mouse.get_pos()):
-        pygame.draw.rect(screen, gray_1, button_3_rect)
-    else:
-        pygame.draw.rect(screen, gray_2, button_3_rect)
 
     screen.blit(text, text_rect)
+    screen.blit(button_surface,(button_1_rect.x, button_1_rect.y))
+    screen.blit(button_surface,(button_2_rect.x, button_2_rect.y))
+    screen.blit(button_surface,(button_3_rect.x, button_3_rect.y))
     screen.blit(text_1,text_1_rect)
     screen.blit(text_2,text_2_rect)
     screen.blit(text_3,text_3_rect)
-    pygame.display.flip()
+    pygame.display.update()
 pygame.quit()
