1b05ba2c6b0656fc818c36e6aba782ade7e0439a
sureock <pulujo2020@gmail.com>
2025-12-08 01:34:23 +0300

    Created one class for all 'text'

--- blackjack/menu.py
+++ blackjack/menu.py
@@ -1,79 +1,115 @@
 import pygame
 
 fps = 24
-white = (255,255,255)
-gray = (25,25,25)
+white = (255, 255, 255)
+gray = (25, 25, 25)
 
 
 def start():
-    button_surface = pygame.Surface((0,0))
+
+    button_surface = pygame.Surface((0, 0))
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     width, height = screen.get_size()
-    width_relative = width/1000
-    height_relative = height/750 
+    width_relative = width / 1000
+    height_relative = height / 750
     pygame.font.match_font('font.otf')
     font = pygame.font.Font('font.otf', 20)
-    clock = pygame.time.Clock()
 
-    text_name = pygame.image.load('logo.png').convert_alpha()
-    text_name_width, text_name_height = text_name.get_size()
-    scale_text_name = pygame.transform.scale(text_name,(text_name_width*width_relative//2,text_name_height*height_relative//2))
-    text_rect = scale_text_name.get_rect()
-    text_rect.topleft = ((width - text_name_width*width_relative//2)//2,20)
+    def get_relative_width(x):
+        return x * width_relative
 
-    text_3 = font.render("EXIT",True,white)
-    text_3_width, text_3_height = text_3.get_size()
-    scale_text_3 = pygame.transform.scale(text_3,(text_3_width*width_relative,text_3_height*height_relative))
-    text_3_rect = scale_text_3.get_rect()
-    text_3_rect.topleft =((width-text_3_width*width_relative)//2,(height-(text_3_height+10)*height_relative))
-    button_3_rect = pygame.Rect((width-text_3_width*width_relative)//2,(height-(text_3_height+10)*height_relative),
-                                text_3_width*width_relative,text_3_height*height_relative)
+    def get_relative_height(x):
+        return x * height_relative
 
-    text_2 = font.render("SETUP",True,white)
-    text_2_width, text_2_height = text_2.get_size()
-    scale_text_2 = pygame.transform.scale(text_2,(text_2_width*width_relative,text_2_height*height_relative))
-    text_2_rect = text_2.get_rect()
-    text_2_rect.topleft =((width-text_2_width*width_relative)//2,(height-(text_3_height+10+text_2_height+5)*height_relative))
-    button_2_rect = pygame.Rect((width-text_2_width*width_relative)//2,(height-(text_3_height+10+text_2_height+5)*height_relative),
-                                text_2_width*width_relative,text_2_height*height_relative)
+    def scale(x, y):
+        return pygame.transform.scale(x, y)
 
-    text_1 = font.render("PLAY",True,white)
-    text_1_width, text_1_height = text_1.get_size()
-    scale_text_1 = pygame.transform.scale(text_1,(text_1_width*width_relative,text_1_height*height_relative))
-    text_1_rect = scale_text_1.get_rect()
-    text_1_rect.topleft =((width-text_1_width*width_relative)//2,(height-(text_3_height+10+text_2_height+5+text_1_height+5)*height_relative))
-    button_1_rect = pygame.Rect((width-text_1_width*width_relative)//2,height-((text_3_height+10+text_2_height+5+text_1_height+5)*height_relative),
-                                text_1_width*width_relative,text_1_height*height_relative)
+    class Text:
+        def __init__(self, text, logo=False):
+            self.text = text
+            self.text_width = text.get_size()[0]
+            self.text_height = text.get_size()[1]
+
+            if logo:
+                self.text_width = self.text_width // 2
+                self.text_height = self.text_height // 2
+
+            self.text_scale = scale(text,
+                                    (get_relative_width(self.text_width),
+                                     get_relative_height(self.text_height)
+                                     ))
+            self.text_rect = self.text_scale.get_rect()
+            self.topleft_param = ((
+                width - get_relative_width(self.text_width)) // 2,
+                                  20)
+            self.text_rect.topleft = self.topleft_param
+
+            self.button_rect = pygame.Rect(self.topleft_param[0],
+                                           self.topleft_param[1],
+                                           get_relative_width(
+                                               self.text_width),
+                                           get_relative_height(
+                                               self.text_height))
+
+        def resize(self, new_param):
+            self.resize_param = new_param
+
+            self.topleft_param = ((
+                width - get_relative_width(self.text_width)) // 2,
+                                  height - get_relative_height(new_param))
+
+            self.text_rect.topleft = self.topleft_param
+
+            self.button_rect = pygame.Rect(self.topleft_param[0],
+                                           self.topleft_param[1],
+                                           get_relative_width(
+                                               self.text_width),
+                                           get_relative_height(
+                                               self.text_height))
+
+    text_name = Text(pygame.image.load('logo.png').convert_alpha(), True)
+
+    text_3 = Text(font.render("EXIT", True, white))
+    text_3.resize(text_3.text_height + 10)
+
+    text_2 = Text(font.render("SETUP", True, white))
+    text_2.resize(text_3.resize_param + text_2.text_height + 5)
+
+    text_1 = Text(font.render("PLAY", True, white))
+    text_1.resize(text_2.resize_param + text_1.text_height + 5)
 
     running = True
     while running:
-        dt = clock.tick(fps)/1000
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
+
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                if button_1_rect.collidepoint(event.pos):
+                if text_1.button_rect.collidepoint(event.pos):
                     play = True
                     running = False
                     setup = False
-                if button_2_rect.collidepoint(event.pos):
+                if text_2.button_rect.collidepoint(event.pos):
                     setup = True
                     running = False
                     play = False
-                if button_3_rect.collidepoint(event.pos):
+                if text_3.button_rect.collidepoint(event.pos):
                     running = False
                     setup = False
                     play = False
 
         screen.fill(gray)
-        screen.blit(scale_text_name, text_rect)
-        screen.blit(button_surface,(button_1_rect.x, button_1_rect.y))
-        screen.blit(button_surface,(button_2_rect.x, button_2_rect.y))
-        screen.blit(button_surface,(button_3_rect.x, button_3_rect.y))
-        screen.blit(scale_text_1,text_1_rect)
-        screen.blit(scale_text_2,text_2_rect)
-        screen.blit(scale_text_3,text_3_rect)
+        screen.blit(text_name.text_scale, text_name.text_rect)
+        screen.blit(button_surface,
+                    (text_1.button_rect.x, text_1.button_rect.y))
+        screen.blit(button_surface,
+                    (text_2.button_rect.x, text_2.button_rect.y))
+        screen.blit(button_surface,
+                    (text_3.button_rect.x, text_3.button_rect.y))
+        screen.blit(text_1.text_scale, text_1.text_rect)
+        screen.blit(text_2.text_scale, text_2.text_rect)
+        screen.blit(text_3.text_scale, text_3.text_rect)
         pygame.display.update()
-    return(running,setup,play)
+
+    return (running, setup, play)
