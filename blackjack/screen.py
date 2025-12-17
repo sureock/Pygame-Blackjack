7507c42ab8f7f7fdc04da83172c7ec4c8b058563
V1rysn1k <ivan.voronko718@gmail.com>
2025-12-17 19:37:46 +0300

    Добавление входа через консоль

--- blackjack/screen.py
+++ blackjack/screen.py
@@ -1,13 +1,29 @@
 import menu
 import pygame
+import parse
+import bdclass
 
-pygame.display.set_caption('Blackjack')
-clock = pygame.time.Clock()
+def call_game(name):
+    pygame.display.set_caption('Blackjack')
+    clock = pygame.time.Clock()
 
-pygame.init()
+    pygame.init()
 
-music = pygame.mixer.music
-music.load("Play Roulette.mp3")
-music.play(loops=-1)
+    music = pygame.mixer.music
+    music.load("Play Roulette.mp3")
+    music.play(loops=-1)
 
-menu.start()
+    menu.start(name)
+
+name, password = parse.get_args()
+auth = bdclass.Auth(name, password)
+
+text, result = auth.login()
+print(text)
+if not result:
+    text, result = auth.regist()
+    print(text)
+    if result:
+        call_game(name)
+else:
+    call_game(name)
