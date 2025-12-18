8e9fbc0f6073d25b399a9a8e162ebceaba56ad29
sureock <pulujo2020@gmail.com>
2025-12-18 17:34:59 +0300

    Changed screen.py to main.py

---
+++ blackjack/main.py
@@ -0,0 +1,34 @@
+import menu
+import pygame
+import parse
+import bdclass
+
+def main():
+    def call_game(name):
+        pygame.display.set_caption('Blackjack')
+
+        pygame.init()
+
+        music = pygame.mixer.music
+        music.load("Play Roulette.mp3")
+        music.play(loops=-1)
+
+        menu.start(name)
+
+
+    name, password = parse.get_args()
+    auth = bdclass.Auth(name, password)
+
+    text, result = auth.login()
+    print(text)
+    if not result:
+        text, result = auth.regist()
+        print(text)
+        if result:
+            call_game(name)
+    else:
+        call_game(name)
+
+
+if __name__ == '__main__':
+    main()
